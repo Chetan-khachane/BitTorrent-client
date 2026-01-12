9b55e8d82fe5e6d355ab528d759e9b6c16451e2d
Chetan-khachane <chetankhachane655@gmail.com>
2026-01-12 18:58:13 +0530

    Printed piece length and piece hash in hexadecimal form

--- src/main.cpp
+++ src/main.cpp
@@ -42,120 +42,6 @@ bool sha1_wincrypt(
 
 using json = nlohmann::json;
 
-//json decode_bencoded_string(const std::string& encoded_value) {
-//    size_t colon_index = encoded_value.find(':');
-//    if (colon_index != std::string::npos) {
-//        std::string number_string = encoded_value.substr(0, colon_index);
-//        int64_t number = std::atoll(number_string.c_str());
-//        std::string str = encoded_value.substr(colon_index + 1, number);
-//        return json(str);
-//    }else {
-//        throw std::runtime_error("Invalid encoded value: " + encoded_value);
-//     }
-//}
-//
-//json decode_bencoded_integer(const std::string& encoded_value) {
-//    return json(std::stol(encoded_value.substr(1, encoded_value.length() - 2)));
-//
-//}
-//
-//json decode_bencoded_dictionary(const std::string& encoded_dict) {
-//    json res = json::object();
-//    for (int i = 1; i < encoded_dict.length() - 1;) {
-// 
-//            int colon = encoded_dict.find(':', i);//2
-//            json value;
-//            int len = std::stoi(encoded_dict.substr(i,colon-i));//3
-//            json key = decode_bencoded_string(encoded_dict.substr(i,len+2));//3:foo i = 1 
-//            i +=  len + 2;
-//            if (encoded_dict[i] == 'i') {
-//				int finish_idx = encoded_dict.find('e', i);
-//                value = decode_bencoded_integer(encoded_dict.substr(i,finish_idx-i + 1));
-//				i = finish_idx + 1;
-//            }
-//            else {
-//                int colon_value = encoded_dict.find(':', i);
-//                int len_value = std::stoi(encoded_dict.substr(i, colon_value - i));
-//				value = decode_bencoded_string(encoded_dict.substr(i,  2 + len_value));
-//                i += len_value + 2;
-//            }
-//           
-//            res[key.get<std::string>()] = value;
-//    }
-//    return res;
-//
-//
-//}
-////list : l
-////dict : d
-////int : i
-////string : number : 
-//
-//std::vector<json> decode_bencoded_list(const std::string& encoded_list) {
-//    std::vector<json> result;
-//
-//    //for (size_t i = 1; i < encoded_list.length() - 1; ) {
-//
-//    //    if (encoded_list[i] == 'i') { // integer
-//    //        int finish_idx = encoded_list.find('e', i);
-//    //        result.push_back(
-//    //            decode_bencoded_integer(
-//    //                encoded_list.substr(i, finish_idx - i + 1)
-//    //            )
-//    //        );
-//    //        i = finish_idx + 1;
-//    //    }
-//    //    else if (std::isdigit(encoded_list[i])) { // string
-//    //        int colon = encoded_list.find(':', i);
-//    //        int len = std::stoi(encoded_list.substr(i, colon - i));
-//
-//    //        result.push_back(
-//    //            decode_bencoded_string(
-//    //                encoded_list.substr(i, (colon - i) + 1 + len)
-//    //            )
-//    //        );
-//
-//    //        i = colon + 1 + len;
-//    //    }
-//    //}
-//    for (int i = 1; i < encoded_list.length() - 1;) {
-//        if (encoded_list[i] == 'i') {//integer
-//            int finish_idx = encoded_list.find('e',i);
-//            result.push_back(decode_bencoded_integer(encoded_list.substr(i,finish_idx-i+1)));
-//            i = finish_idx + 1;
-//        }
-//        else if (encoded_list[i] == 'l') {//list
-//
-//        }
-//        else if (encoded_list[i] == 'd') {//dictionary
-//
-//        }
-//        else {//string
-//
-//        }
-//    }
-//    return result;
-//}
-//
-////parse_bencode
-//
-//json parse_bencode(const std::string& encoded_value) {
-//    if (std::isdigit(encoded_value[0])) {
-//        // Example: "5:hello" -> "hello"
-//		   return decode_bencoded_string(encoded_value);
-//    }
-//    else if (encoded_value[0] == 'i' && encoded_value.back() == 'e') {
-//		return decode_bencoded_integer(encoded_value);
-//    }
-//    else if (encoded_value[0] == 'l' && encoded_value.back() == 'e') {
-//        return decode_bencoded_list(encoded_value);
-//    }else if(encoded_value[0] == 'd' && encoded_value.back() == 'e') {
-//        return decode_bencoded_dictionary(encoded_value);
-//	}
-//    else {
-//        throw std::runtime_error("Unhandled encoded value: " + encoded_value);
-//    }
-//}
 
 //Bencode Parser Design
 
@@ -329,7 +215,6 @@ public:
             out.insert(out.end(), s.begin(), s.end());
         }
         else if (j.is_binary()) {
-            // IMPORTANT: handles "pieces" correctly
             const auto& b = (j.get_binary());
             auto len = std::to_string(b.size());
             out.insert(out.end(), len.begin(), len.end());
@@ -405,12 +290,34 @@ int main(int argc, char* argv[]) {
             info_hash
         );
         
-        std::cout << "Tracker URL : " << torrent_data["announce"] << "Length : "<<torrent_data["info"]["length"] << "\n";
+        std::cout << "Tracker URL : " << torrent_data["announce"] << "\nLength : "<<torrent_data["info"]["length"] << "\n";
         std::cout << "Info Hash: ";
         for (int i = 0; i < 20; i++) {
             printf("%02x", info_hash[i]);
         }
 		std::cout << std::endl;
+        std::cout << "Piece Length : " << torrent_data["info"]["piece length"] << "\n";
+		std::cout << "Piece Hashes : \n";
+       
+        std::string pieces_str = torrent_data["info"]["pieces"].get<std::string>();
+
+
+        if (pieces_str.empty()) {
+            printf("ERROR: pieces is empty\n");
+            return 1;
+        }
+
+        if (pieces_str.size() % 20 != 0) {
+            printf("ERROR: invalid pieces size\n");
+            return 1;
+        }
+
+        size_t idx = 0;
+        for (uint8_t b : pieces_str) {
+            printf("%02x", b);
+            if (++idx % 20 == 0)
+                printf("\n");
+        }
     }
     else {
         std::cerr << "unknown command: " << command << std::endl;
